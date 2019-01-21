1446ed62903c92c129f92b82d9246f97abe9d650
chavdar <chavdar@chavo.com>
2019-01-21 19:39:06 +0200

    add-service

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,6 @@ Vagrant.configure(2) do |config|
       config.vm.define "consul0#{n}" do |box|
         box.vm.hostname = "consul0#{n}"
         box.vm.network "private_network", ip: "192.168.56.#{50+n}"
-        box.vm.network "forwarded_port", guest: 8500, host: 8500 + n
         box.vm.provision "shell",inline: "cd /vagrant ; bash scripts/consul.sh", env: {"SERVER_COUNT" => SERVER_COUNT}
 
       end
--- scripts/consul.sh
+++ scripts/consul.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update -y
-apt-get install unzip socat jq dnsutils -y 
+apt-get install unzip socat jq dnsutils curl -y 
 
 SERVER_COUNT=${SERVER_COUNT}
 
@@ -14,14 +14,20 @@ rm -fr consul_1.4.0_linux_amd64.zip
 popd
 }
 
+cd /vagrant
 # Starting consul
 killall consul
 IPs=$(hostname -I | cut -f2 -d' ')
 
-
 set +x
 
-consul agent -server -ui -bind 0.0.0.0 -advertise $IPs -client 0.0.0.0 -data-dir=/tmp/consul -bootstrap-expect=$SERVER_COUNT -retry-join=192.168.56.52 -retry-join=192.168.56.51 > /tmp/consul.log & 
-        
+cd /vagrant
+consul agent -server -ui -bootstrap-expect=$SERVER_COUNT \
+    -data-dir=/tmp/consul -bind 0.0.0.0 -advertise $IPs -client 0.0.0.0 \
+    -enable-script-checks=true -config-dir=consul.d -retry-join=192.168.56.52 -retry-join=192.168.56.51> /tmp/consul.log &
 sleep 5
 set +x
+
+consul reload
+
+consul members
