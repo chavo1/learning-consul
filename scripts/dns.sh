e8281489dd76930611be552abcc9b83bb5610b3a
chavo1 <chavdar.rakov@gmail.com>
2019-01-23 16:41:16 +0200

    optimize

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-apt-get update -y
 which dnsmasq &>/dev/null || {
 apt-get install dnsmasq -y 
 }
 
-# Consul DNS to be resolved as well as external
-
+#######################################################################
+# Consul DNS, to be resolved in the consul domain as well as external #
+#######################################################################
 sudo echo "server=/consul/127.0.0.1#8600" > /etc/dnsmasq.d/10-consul
 
 sudo sed -i 's/#resolv-file=/resolv\-file=\/etc\/dnsmasq.d\/external.conf/g' /etc/dnsmasq.conf
 
-cat << EOF > /etc/dnsmasq.d/external.conf
+cat <<EOF > /etc/dnsmasq.d/external.conf
 server=8.8.8.8
 EOF
 
 sudo systemctl restart dnsmasq
+
